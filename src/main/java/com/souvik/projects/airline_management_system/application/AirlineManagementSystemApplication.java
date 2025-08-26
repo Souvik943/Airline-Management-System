9e20737b8f3b5cb1a5d00ce0415fee6d4944b358
souvik <souvikkarmakar2k19@gmail.com>
2025-08-26 19:53:12 +0530

    Airline Management System : Initial Commit (Project setup)

---
+++ src/main/java/com/souvik/projects/airline_management_system/application/AirlineManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.souvik.projects.airline_management_system.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineManagementSystemApplication.class, args);
+	}
+
+}
